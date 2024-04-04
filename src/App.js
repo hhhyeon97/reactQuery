a966fe21528994f7bfe8990956a560be57e0aa35
jihyeon <kth083030@naver.com>
2024-04-04 22:43:07 +0900

    캐시캐시

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import './App.css';
 import HomePage from './page/HomePage';
+import NormalPage from './page/NormalPage';
 import ReactQueryPage from './page/ReactQueryPage';
 import { Routes, Route, Link } from 'react-router-dom';
 function App() {
   return (
-    <div>
+    <div className="wrap">
       <nav style={{ backgroundColor: '#92b3f0', padding: '20px' }}>
         <Link to="/" style={{ margin: '20px' }}>
           Home
+        </Link>
+        <Link to="/normal" style={{ margin: '20px' }}>
+          Normal
         </Link>
         <Link to="/react-query">React Query</Link>
       </nav>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/normal" element={<NormalPage />} />
         <Route path="/react-query" element={<ReactQueryPage />} />
       </Routes>
     </div>
--- src/page/ReactQueryPage.js
+++ src/page/ReactQueryPage.js
@@ -7,6 +7,13 @@ import { useQuery } from '@tanstack/react-query';
 // 리액트 쿼리 대표적인 기능
 // useQuery 훅
 // useQuery는 컴포넌트가 시작할 때 바로 실행된다.(실행시점설정가능)
+// useQuery는 받아온 데이터를 캐시에 저장함 ->
+// posts 라는 이름의 api를 호출 할 때 기존에 저장되어 있는 캐시정보를 먼저 보여주니까
+// 로딩을 안 보여줘도 되면서 유저 경험적으로 더 좋아지게 됨 !
+// api호출은 캐시가 있어도 진행을 함 !! 로딩 대신에 캐시된 데이터를 미리 보여주는 것일 뿐
+// 뒤에서는 api 호출 작업을 진행한다. api 호출이 완료가 되면 다시 캐시를 업데이트 함
+// == > 캐시는 새로운 데이터가 오면 계속 업데이트가 되고 있음
+// 캐시 수명 조절하는 옵션 gcTime
 
 const ReactQueryPage = () => {
   const { isLoading, data, isError, error } = useQuery({
@@ -18,6 +25,7 @@ const ReactQueryPage = () => {
     select: (data) => {
       return data.data;
     },
+    gcTime: 3000,
   });
   console.log('ddd', data, isLoading);
   console.log('error', isError, error);
