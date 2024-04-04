540a00bb5aaf70cda52ba54d48ee235a3b8302ba
jihyeon <kth083030@naver.com>
2024-04-04 20:41:30 +0900

    프로젝트셋팅

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Home from './page/Home';
+import ReactQuery from './page/ReactQuery';
+import { Routes, Route, Link } from 'react-router-dom';
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <nav style={{ backgroundColor: '#92b3f0', padding: '20px' }}>
+        <Link to="/" style={{ margin: '20px' }}>
+          Home
+        </Link>
+        <Link to="/react-query">React Query</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/react-query" element={<ReactQuery />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/page/ReactQuery.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ReactQuery = () => {
+  return <div>React Query</div>;
+};
+
+export default ReactQuery;
